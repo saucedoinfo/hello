7f79b3ebf51d61aefdf2f44ab2611e9b7cd2626e
Fernando S. Saucedo <saucedo.info@gmail.com>
2021-03-14 18:02:07 -0300

    Se configura gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -35,8 +35,8 @@ import { init as server, stream, reload } from "browser-sync";
 //Plumber
 import plumber from "gulp-plumber";
 
-//Typescript
-import ts from "gulp-typescript";
+// //Typescript
+// import ts from "gulp-typescript";
 
 const production = false;
 
@@ -155,9 +155,9 @@ gulp.task("default", () => {
 	server({
 		server: "./public",
 	});
-	// gulp.watch('./src/*.html', gulp.series('html-min')).on('change', reload)
+	gulp.watch('./src/*.html', gulp.series('html-min')).on('change', reload)
 	// gulp.watch('./src/css/*.css', gulp.series('styles'))
-	gulp.watch("./src/views/**/*.pug", gulp.series("views")).on("change", reload);
+	// gulp.watch("./src/views/**/*.pug", gulp.series("views")).on("change", reload);
 	gulp.watch("./src/scss/**/*.scss", gulp.series("sass"));
 	//   gulp.watch('./src/js/*.js', gulp.series('babel')).on('change', reload);
 	gulp.watch("./src/ts/*.ts", gulp.series("typescript")).on("change", reload);
