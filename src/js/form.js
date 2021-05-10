451e850a3cbf46a7744a200b813de56b4e0b3382
Fernando S. Saucedo <saucedo.info@gmail.com>
2021-05-10 12:39:15 -0300

    fix form center

--- src/js/form.js
+++ src/js/form.js
@@ -1,26 +1,3 @@
-// Tool https://formspree.io/
-
-// const $form = document.querySelector('#form')
-
-// $form.addEventListener('submit', handleSubmit)
-
-// async function handleSubmit(event) {
-//     event.preventDefault()
-//     const form = new FormData(this)
-//     const response = await fetch(this.action, {
-//         method: this.method,
-//         body: form,
-//         headers: {
-//             'Accept': 'application/json'
-//         }
-//     })
-//     if (response.ok) {
-//         this.reset()
-//         alert('Gracias por contactarme. En breve te contestare')
-//     }
-// }
-
-
 // Tool https://web3forms.com/
 const form = document.getElementById('form');
 const result = document.getElementById('result');
